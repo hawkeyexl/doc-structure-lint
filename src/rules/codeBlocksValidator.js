ec5092618625e38c6342295a092d5eeb72ba37fc
hawkeyexl <hawkeyexl@gmail.com>
2024-10-23 11:27:21 -0700

    Fix code blocks and paragraphs validation to use length for min/max checks

--- src/rules/codeBlocksValidator.js
+++ src/rules/codeBlocksValidator.js
@@ -2,9 +2,10 @@ export function validateCodeBlocks(section, template) {
   const errors = [];
 
   if (template.code_blocks) {
+    // Max
     if (
       template.code_blocks.min &&
-      section.code_blocks < template.code_blocks.min
+      section.code_blocks.length < template.code_blocks.min
     ) {
       errors.push({
         head: section.title,
@@ -13,9 +14,11 @@ export function validateCodeBlocks(section, template) {
         message: `Expected at least ${template.code_blocks.min} code blocks, but found ${section.code_blocks}`,
       });
     }
+
+    // Min
     if (
       template.code_blocks.max &&
-      section.code_blocks > template.code_blocks.max
+      section.code_blocks.length > template.code_blocks.max
     ) {
       errors.push({
         head: section.title,
--- src/rules/paragraphsValidator.js
+++ src/rules/paragraphsValidator.js
@@ -2,26 +2,29 @@ export function validateParagraphs(section, template) {
   const errors = [];
 
   if (template.paragraphs) {
+    // Max
     if (
       template.paragraphs.min &&
-      section.paragraphs < template.paragraphs.min
+      section.paragraphs.length < template.paragraphs.min
     ) {
       errors.push({
         head: section.title,
         startIndex: section.startIndex,
         endIndex: section.endIndex,
-        message: `Expected at least ${template.paragraphs.min} paragraphs, but found ${section.paragraphs}`,
+        message: `Expected at least ${template.paragraphs.min} paragraphs, but found ${section.paragraphs.length}`,
       });
     }
+
+    // Min
     if (
       template.paragraphs.max &&
-      section.paragraphs > template.paragraphs.max
+      section.paragraphs.length > template.paragraphs.max
     ) {
       errors.push({
         head: section.title,
         startIndex: section.startIndex,
         endIndex: section.endIndex,
-        message: `Expected at most ${template.paragraphs.max} paragraphs, but found ${section.paragraphs}`,
+        message: `Expected at most ${template.paragraphs.max} paragraphs, but found ${section.paragraphs.length}`,
       });
     }
   }
